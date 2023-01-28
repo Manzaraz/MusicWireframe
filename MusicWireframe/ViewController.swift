363a274ba2bd533c473dda1558aa8f8ad8ddfebf
Christian Manzaraz <magnusmanz@gmail.com>
2023-01-27 23:36:23 -0300

    Set Up the View and Add Outlets

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var albumImageView: UIImageView!
+    
+    @IBOutlet var reverseButton: UIButton!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var forwardButton: UIButton!
+    
+    @IBOutlet var reverseBackground: UIView!
+    @IBOutlet var playPauseBackground: UIView!    
+    @IBOutlet var forwardBackground: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
